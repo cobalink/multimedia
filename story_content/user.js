e3cec823761f47441d1aabb94cfb61e85ecba2d5
cobalink <potoajaw@gmail.com>
2025-12-29 15:24:11 +0700

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,21 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5WwHedxwa6Q":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.src="musik.mp3";
+
+audio.load();
+
+audio.play();
+}
+
